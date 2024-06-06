b656ec6b7b9d483dca36a796f5bd6baa9bee24e2
IsmoilSafarovFullStack <safarovismoil716@gmail.com>
2024-06-06 21:14:29 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>{{ message }}</h1>
+	</div>
+</template>
+<script setup lang="ts">
+import axios from 'axios'
+const message = ref('')
+
+const restAPI = async () => {
+	try {
+		const response = await axios.get('http://localhost:3000/api/hello')
+		message.value = response.data
+	} catch (error) {
+		console.log('error: ', error)
+	}
+}
+onMounted(restAPI)
+</script>
